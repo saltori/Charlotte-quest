05279d2fa5a135d8a4b403eb3657a35b886e28a1
脱け殻 <0bs5p05v38p3-4w@ezweb.ne.jp>
2019-03-26 17:03:49 +0900

    コミット

---
+++ しゃるクエ！/Assets/テスト用スクリプト/mapChar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mapChar : MonoBehaviour {
+
+    public static float posx, posy;
+    public SpriteRenderer charImage;
+    // Use this for initialization
+    void Start()
+    {
+        charImage = GetComponent<SpriteRenderer>();
+        
+    }
+    
+	// Update is called once per frame
+	void Update () {
+        charImage.transform.position = new Vector3(posx, posy);
+    }
+}
